0ef36a167e7bb1460be314369f68b89b1844c97a
k-guti14 <naka_kento_1207@icloud.com>
2023-02-05 16:54:23 +0900

    ログイン画面作成

---
+++ html/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog</title>
+</head>
+<body>
+<form>
+    <h1 class="h3 mb-3 fw-normal">ログイン画面</h1>
+    <label class="visually-hidden">ユーザ名</label>
+    <input type="text" name="name" class="form-control" placeholder="ユーザ名" required autofocus>
+    <label class="visually-hidden">パスワード</label>
+    <input type="password" name="password" class="form-control" placeholder="パスワード" required>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">ログインする</button>
+  </form>
+</body>
+</html>
